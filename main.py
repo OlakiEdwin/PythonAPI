8c8a2cb0bd64bbf95743f5ef939928b416e1acc1
Olaki Edwin <olakiedwin@gmail.com>
2025-06-01 22:41:50 +0300

    Added POST method

--- main.py
+++ main.py
@@ -16,10 +16,20 @@ def get_user(user_id):
 
     return jsonify(user_data), 200
 
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 #GET  #Request data from a specified resource
 #POST #Create a resource
 #PUT #Update a resource
 #DELETE #Delete a resource
-
-if __name__ == "__main__":
-    app.run(debug=True)
