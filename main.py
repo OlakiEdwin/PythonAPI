d4a7c59c63ac199c7d572fab5c987b6a338c2319
Olaki Edwin <olakiedwin@gmail.com>
2025-06-01 22:14:21 +0300

    Add GET routes

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Home"
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "Olaki Edwin",
+        "email": "olakiedwin@gmail.com"
+    }
 
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
+#GET  #Request data from a specified resource
+#POST #Create a resource
+#PUT #Update a resource
+#DELETE #Delete a resource
 
 if __name__ == "__main__":
     app.run(debug=True)
